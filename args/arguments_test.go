3bb2ab21fcfdaa80fff6bd8160e9bf1aad025cf2
denis96z <zinoviev.denis@yandex.ru>
2018-10-26 13:20:00 +0300

    args package added

---
+++ args/arguments_test.go
@@ -0,0 +1,66 @@
+package args
+
+import (
+	"regexp"
+	"testing"
+)
+
+func TestGetString(t *testing.T) {
+	osArgs = []string{
+		"--flag",
+		"--address=localhost:8080",
+	}
+
+	if GetString("address", "localhost") != "localhost:8080" {
+		t.Error("argument not parsed")
+	}
+
+	if GetString("flag", "default") != "default" {
+		t.Error("default value not returned")
+	}
+
+	if GetString("someVar", "default") != "default" {
+		t.Error("default value not returned")
+	}
+}
+
+func TestGetFlag(t *testing.T) {
+	osArgs = []string{
+		"--flag",
+		"--address=localhost:8080",
+	}
+
+	if GetFlag("flag", false) != true {
+		t.Error("flag not parsed")
+	}
+
+	if GetFlag("address", false) != false {
+		t.Error("wrong flag parsed")
+	}
+
+	if GetFlag("unknown", false) != false {
+		t.Error("unknown flag parsed")
+	}
+}
+
+func TestParseGroups(t *testing.T) {
+	r, _ := regexp.Compile(`^(?P<name>\w+)$`)
+
+	str := "user"
+
+	mp := parseGroups(r, str)
+	if len(mp) != 2 {
+		t.Error("wrong number of groups")
+	}
+
+	if mp[""] != str || mp["name"] != str {
+		t.Error("wrong group value")
+	}
+
+	str = "--user"
+
+	mp = parseGroups(r, str)
+	if mp != nil {
+		t.Error("wrong number of groups")
+	}
+}
