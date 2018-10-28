3b12e4bcde42a59996b732d5b31be9ff43555c40
denis96z <zinoviev.denis@yandex.ru>
2018-10-28 20:06:19 +0300

    refactoring. glide -> dep. some methods of Logger implemented

--- log/logger.go
+++ log/logger.go
@@ -14,6 +14,18 @@ func NewLogger() *Logger {
 	}
 }
 
+func (log *Logger) Info(args ...interface{}) {
+	log.logger.Info(args...)
+}
+
+func (log *Logger) Error(args ...interface{}) {
+	log.logger.Error(args...)
+}
+
+func (log *Logger) Fatal(args ...interface{}) {
+	log.logger.Fatal(args...)
+}
+
 func (log *Logger) AsLogrusLogger() *logrus.Logger {
 	return log.logger
 }
--- log/logger_test.go
+++ log/logger_test.go
@@ -1,6 +1,9 @@
 package log
 
 import (
+	"bou.ke/monkey"
+	"github.com/sirupsen/logrus"
+	"reflect"
 	"testing"
 )
 
@@ -8,6 +11,42 @@ func TestNewLogger(t *testing.T) {
 	logger := NewLogger()
 	if logger == nil {
 		t.Error("logger not created")
+	}
+
+	infoCalled := false
+	monkey.PatchInstanceMethod(reflect.TypeOf(logger.logger), "Info",
+		func(_ *logrus.Logger, _ ...interface{}) {
+			infoCalled = true
+		},
+	)
+
+	logger.Info("info")
+	if !infoCalled {
+		t.Error("info method not called")
+	}
+
+	errorCalled := false
+	monkey.PatchInstanceMethod(reflect.TypeOf(logger.logger), "Error",
+		func(_ *logrus.Logger, _ ...interface{}) {
+			errorCalled = true
+		},
+	)
+
+	logger.Error("error")
+	if !errorCalled {
+		t.Error("error method not called")
+	}
+
+	fatalCalled := false
+	monkey.PatchInstanceMethod(reflect.TypeOf(logger.logger), "Fatal",
+		func(_ *logrus.Logger, _ ...interface{}) {
+			fatalCalled = true
+		},
+	)
+
+	logger.Fatal("fatal")
+	if !fatalCalled {
+		t.Error("fatal method not called")
 	}
 
 	if logger.AsLogrusLogger() == nil {
