d98132c4df5985d9a6fcc91f5406d8d1fb8387dc
denis96z <zinoviev.denis@yandex.ru>
2018-11-27 22:49:09 +0300

    credentials added

---
+++ auth/credentials.go
@@ -0,0 +1,22 @@
+package auth
+
+import (
+	"github.com/bombergame/common/consts"
+	"github.com/bombergame/common/errs"
+)
+
+//easyjson:json
+type Credentials struct {
+	Username string `json:"username"`
+	Password string `json:"password"`
+}
+
+func (c Credentials) Validate() error {
+	if c.Username == consts.EmptyString {
+		return errs.NewInvalidFormatError("empty username")
+	}
+	if c.Password == consts.EmptyString {
+		return errs.NewInvalidFormatError("empty password")
+	}
+	return nil
+}
